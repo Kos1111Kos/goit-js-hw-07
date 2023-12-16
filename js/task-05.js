0f97333a8ee2e483c0e75fb67e7949356dfa5814
Kos1111Kos <konstantin.smirnov.2011@gmail.com>
2023-12-16 21:44:27 +0200

    task-6

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widget = document.querySelector(".widget");
+const colorSpan = widget.querySelector(".color");
+const changeColorBtn = widget.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomColor();
+  widget.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
