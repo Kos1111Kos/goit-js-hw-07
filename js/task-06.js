f1b6bf614b1d610f5b98031b8b1b2a7bebd3e119
Kos1111Kos <konstantin.smirnov.2011@gmail.com>
2023-12-16 23:29:48 +0200

    update task 6

--- js/task-06.js
+++ js/task-06.js
@@ -10,34 +10,29 @@ const createBtn = controls.querySelector("[data-create]");
 const destroyBtn = controls.querySelector("[data-destroy]");
 const boxesContainer = document.getElementById("boxes");
 
-createBtn.addEventListener("click", createBoxes);
+createBtn.addEventListener("click", () => createBoxes(Number(input.value)));
 destroyBtn.addEventListener("click", destroyBoxes);
 
-function createBoxes() {
-  const amount = input.value;
+function createBoxes(amount) {
   if (amount < 1 || amount > 100) {
-    alert("Please enter a number between 1 and 100");
+    const message = "Please enter a number between 1 and 100";
+    boxesContainer.innerHTML += `<p class="error-message">${message}</p>`;
     return;
   }
+
+  boxesContainer.innerHTML = "";
+
   for (let i = 0; i < amount; i++) {
     const box = document.createElement("div");
     box.style.width = `${30 + i * 10}px`;
     box.style.height = `${30 + i * 10}px`;
-    box.style.backgroundColor = getRandomColor();
+    box.style.backgroundColor = getRandomHexColor();
     boxesContainer.appendChild(box);
   }
+
   input.value = "";
 }
 
 function destroyBoxes() {
   boxesContainer.innerHTML = "";
-}
-
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
 }
